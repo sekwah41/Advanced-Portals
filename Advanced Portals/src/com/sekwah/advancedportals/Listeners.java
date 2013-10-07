c2032f79790555dc66a646192ed563dc28395c68
Sekwah <contact@sekwah.com>
2013-10-07 21:26:03 +0100

    Started adding the axe interaction code

--- Advanced Portals/src/com/sekwah/advancedportals/Listeners.java
+++ Advanced Portals/src/com/sekwah/advancedportals/Listeners.java
@@ -1,9 +1,39 @@
 package com.sekwah.advancedportals;
 
-import org.bukkit.event.Listener;
+import net.minecraft.server.v1_6_R3.Item;
 
-public class Listeners implements Listener{
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class Listeners implements Listener {
 	
-	
+    @EventHandler
+    public void onMoveEvent(PlayerMoveEvent event) {
+    	// will check if the player is in the portal or not.
+    	
+    }
+    
+    
+    
+    
+    @EventHandler
+    public void oniteminteract(PlayerInteractEvent event) {
+    	// will detect if the player is using an axe so the points of a portal can be set
+    	// also any other detections such as sign interaction or basic block protection
+    	if(event.getPlayer().getItemInHand().getTypeId() == Material.IRON_AXE.getId()) {
+    		if(event.getAction() == Action.LEFT_CLICK_BLOCK){
+    			
+    		}
+    		else if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+    			
+    		}
+    	}
+    }
+    
+    
 	
 }
