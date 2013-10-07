c5f79bb3c0dea903b6f4fa132685497724721d83
sekwah41 <alastair.hawkes@gmail.com>
2013-10-07 20:00:36 +0100

    Added the plugin details and also the onEnable and onDisable functions

--- Advanced Portals/src/com/sekwah/advancedportals/AdvancedPortalsPlugin.java
+++ Advanced Portals/src/com/sekwah/advancedportals/AdvancedPortalsPlugin.java
@@ -3,5 +3,16 @@ package com.sekwah.advancedportals;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdvancedPortalsPlugin extends JavaPlugin {
-
+	
+    @Override
+    public void onEnable(){
+    	
+    }
+    
+    
+    @Override
+    public void onDisable(){
+    	
+    }
+	
 }
