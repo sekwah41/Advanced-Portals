d72f5184e86efb9e97ac9b0913af0a1d7a74282d
sekwah41 <alastair.hawkes@gmail.com>
2013-10-07 20:10:40 +0100

    Added enable and disable messages and also started the listener

--- Advanced Portals/src/com/sekwah/advancedportals/AdvancedPortalsPlugin.java
+++ Advanced Portals/src/com/sekwah/advancedportals/AdvancedPortalsPlugin.java
@@ -3,5 +3,15 @@ package com.sekwah.advancedportals;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdvancedPortalsPlugin extends JavaPlugin {
-
+	
+	public void onEnable() {
+		getLogger().info("\n\nAdvanced portals are being enabled!\n\n");
+	}
+	
+	
+	public void onDisable() {
+		getLogger().info("\n\nAdvanced portals are being disabled!\n\n");
+	}
+	
+	
 }
