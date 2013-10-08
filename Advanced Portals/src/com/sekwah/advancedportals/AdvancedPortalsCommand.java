18f8c8463af07a5cb6c27f84b8bc1b73a5b52aaa
sekwah41 <alastair.hawkes@gmail.com>
2013-10-08 22:22:03 +0100

    Added a command to get a portal region selector and started making the config for the plugin so you can change what you want such as being able to still use the normal axe if wanted or needed.

---
+++ Advanced Portals/src/com/sekwah/advancedportals/AdvancedPortalsCommand.java
@@ -0,0 +1,54 @@
+package com.sekwah.advancedportals;
+
+import java.util.Arrays;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class AdvancedPortalsCommand implements CommandExecutor {
+	
+	private AdvancedPortalsPlugin plugin;
+
+	public AdvancedPortalsCommand(AdvancedPortalsPlugin plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String command, String[] args) {
+		Player player = (Player)sender;
+		ConfigAccessor config = new ConfigAccessor(plugin, "Config.yml");
+		if(args.length > 0){
+			if(args[0].toLowerCase().equals("wand") || args[0].toLowerCase().equals("selector")){
+				if(sender.hasPermission("AdvancedPortals.create")){
+					PlayerInventory inventory = player.getInventory();
+					
+					ItemStack shopitem = new ItemStack(Material.IRON_AXE);
+					ItemMeta shopname = shopitem.getItemMeta();
+					shopname.setDisplayName("§ePortal Region Selector");
+					shopname.setLore(Arrays.asList("§rThis iron axe with has the power to help"
+							, "§r create portals bistowed upon it!"));
+					shopitem.setItemMeta(shopname);
+					
+					inventory.addItem(shopitem);
+					sender.sendMessage("§a[§7AdvancedPortals§a] You have been given a §ePortal Region Selector§a!");
+				}
+				else{
+					sender.sendMessage("§c[§7AdvancedPortals§c] You do not have permission to create portals so you cannot give yourself a §ePortal Region Selector§c!");
+				}
+			}
+		}
+		else{
+			sender.sendMessage("§c[§7AdvancedPortals§c] You need to type something after /" + command + "\n"
+					+ "if you do not know what you can put or would like some help with the commands please type /" + command + " help");
+		}
+		
+		return true;
+	}
+
+}
