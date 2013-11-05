56e1c8b824fac92985222006dda307b69987eb84
Sekwah <contact@sekwah.com>
2013-11-05 20:35:45 +0000

    Started the destinations

--- Advanced Portals/src/com/sekwah/advancedportals/destinations/Destinations.java
+++ Advanced Portals/src/com/sekwah/advancedportals/destinations/Destinations.java
@@ -1,5 +1,23 @@
 package com.sekwah.advancedportals.destinations;
 
-public class Destinations {
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
+public class Destinations {
+	
+	public void create(Player creator, Location pos1, String name){
+		
+	}
+	
+	public void move(Player creator, Location pos1, String name){
+		
+	}
+	
+	public void rename(Player creator, String oldName, String newName){
+		
+	}
+	
+	public void remove(Player player, String name){
+		
+	}
 }
