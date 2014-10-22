0a5f8ca99b8173bc2eb4f9d78a3d36b8a4e8f528
Sekwah <contact@sekwah.com>
2014-10-22 17:48:24 +0100

    Started making the refactors

--- Advanced Portals/src/com/sekwah/advancedportals/AdvancedPortalsPlugin.java
+++ Advanced Portals/src/com/sekwah/advancedportals/AdvancedPortalsPlugin.java
@@ -1,16 +1,20 @@
 package com.sekwah.advancedportals;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sekwah.advancedportals.DataCollector.DataCollector;
+import com.sekwah.advancedportals.compat.NMS;
 import com.sekwah.advancedportals.destinations.Destination;
 import com.sekwah.advancedportals.metrics.Metrics;
 import com.sekwah.advancedportals.portalcontrolls.Portal;
 
 public class AdvancedPortalsPlugin extends JavaPlugin {
-
+	
+	public NMS nmsAccess;
+	
 	public void onEnable() {
 		
 		new Assets(this);
@@ -60,6 +64,41 @@ public class AdvancedPortalsPlugin extends JavaPlugin {
 		
 		this.getServer().getConsoleSender().sendMessage("§aAdvanced portals have been sucsessfully enabled!");
 		
+		String packageName = getServer().getClass().getPackage().getName();
+		String[] packageSplit = packageName.split("\\.");
+		String version = packageSplit[packageSplit.length - 1];
+
+		try {
+			Class<?> nmsClass = Class.forName("com.sekwah.advancedportals.compat." + version);
+			if(NMS.class.isAssignableFrom(nmsClass)){
+				this.nmsAccess = (NMS) nmsClass.getConstructor().newInstance();
+			}else
+			{
+				System.out.println("Something went wrong, please notify the author and tell them this version v:" + version);
+				this.setEnabled(false);
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("This version of craftbukkit is not yet supported, please notify the author and give them this version v:" + version);
+			this.setEnabled(false);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
