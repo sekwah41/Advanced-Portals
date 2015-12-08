5ecdc7787a053a9add57cf5423515ce49675f660
Sekwah <contact@sekwah.com>
2015-12-08 14:28:33 +0000

    Catching some null errors

--- src/com/sekwah/advancedportals/metrics/Metrics.java
+++ src/com/sekwah/advancedportals/metrics/Metrics.java
@@ -321,7 +321,7 @@ public class Metrics {
         boolean onlineMode = Bukkit.getServer().getOnlineMode(); // TRUE if online mode is enabled
         String pluginVersion = description.getVersion();
         String serverVersion = Bukkit.getVersion();
-        int playersOnline = Bukkit.getServer().getOnlinePlayers().length;
+        int playersOnline = Bukkit.getServer().getOnlinePlayers().size();//Bukkit.getServer().getOnlinePlayers().length;
 
         // END server software specific section -- all code below does not use any code outside of this class / Java
 
