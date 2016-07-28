09ac2adf060253fcdd8a22f836b4b26f96621d52
Alastair <alastair.hawkes@gmail.com>
2016-07-28 23:09:30 +0100

    started adding tag registry and updated to support 1.10_R1

--- src/com/sekwah/advancedportals/portals/AdvancedPortal.java
+++ src/com/sekwah/advancedportals/portals/AdvancedPortal.java
@@ -1,5 +1,6 @@
 package com.sekwah.advancedportals.portals;
 
+import com.sekwah.advancedportals.api.portaldata.PortalArg;
 import java.util.HashSet;
 import org.bukkit.Location;
 import org.bukkit.Material;
