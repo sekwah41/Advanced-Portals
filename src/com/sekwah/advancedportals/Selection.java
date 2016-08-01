94216800ab1dd75b26e6874aae3061f913bac59a
Max Qian <public@maxqia.com>
2016-07-31 17:22:58 -0700

    Show selected portal and deduplicate code

--- src/com/sekwah/advancedportals/Selection.java
+++ src/com/sekwah/advancedportals/Selection.java
@@ -34,85 +34,6 @@ public class Selection {
     @SuppressWarnings("deprecation")
     public static void Show(final Player player, final AdvancedPortalsPlugin plugin, String portalName) {
 
-        ConfigAccessor portalConfig = new ConfigAccessor(plugin, "portals.yml");
-
-        int LowX = portalConfig.getConfig().getInt(portalName + ".pos2.X");
-        int LowY = portalConfig.getConfig().getInt(portalName + ".pos2.Y");
-        int LowZ = portalConfig.getConfig().getInt(portalName + ".pos2.Z");
-
-        int HighX = portalConfig.getConfig().getInt(portalName + ".pos1.X");
-        int HighY = portalConfig.getConfig().getInt(portalName + ".pos1.Y");
-        int HighZ = portalConfig.getConfig().getInt(portalName + ".pos1.Z");
-
-        final Location pos1 = new Location(player.getWorld(), LowX, LowY, LowZ);
-        final Location pos2 = new Location(player.getWorld(), HighX, HighY, HighZ);
-
-		/*
-		 * There are alot of for loops at the moment, when i find an easier way to do these other that a load of if statements
-		 * then i will change it, but for now its the best way i can think of for doing this. 
-		 */
-
-        for (int x = LowX; x <= HighX; x++) {
-            Location loc = new Location(player.getWorld(), x, LowY, LowZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int x = LowX; x <= HighX; x++) {
-            Location loc = new Location(player.getWorld(), x, LowY, HighZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int z = LowZ; z <= HighZ; z++) {
-            Location loc = new Location(player.getWorld(), LowX, LowY, z);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int z = LowZ; z <= HighZ; z++) {
-            Location loc = new Location(player.getWorld(), HighX, LowY, z);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int y = LowY; y <= HighY; y++) {
-            Location loc = new Location(player.getWorld(), LowX, y, LowZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int y = LowY; y <= HighY; y++) {
-            Location loc = new Location(player.getWorld(), LowX, y, HighZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int y = LowY; y <= HighY; y++) {
-            Location loc = new Location(player.getWorld(), HighX, y, LowZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int y = LowY; y <= HighY; y++) {
-            Location loc = new Location(player.getWorld(), HighX, y, HighZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int x = LowX; x <= HighX; x++) {
-            Location loc = new Location(player.getWorld(), x, HighY, HighZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int x = LowX; x <= HighX; x++) {
-            Location loc = new Location(player.getWorld(), x, HighY, LowZ);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int z = LowZ; z <= HighZ; z++) {
-            Location loc = new Location(player.getWorld(), LowX, HighY, z);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-        for (int z = LowZ; z <= HighZ; z++) {
-            Location loc = new Location(player.getWorld(), HighX, HighY, z);
-            player.sendBlockChange(loc, blockType, metadata);
-        }
-
-
-        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-            public void run() {
-                Selection.Hide(player, plugin, pos1, pos2);
-            }
-        }, timeout * 20);
-
-    }
-
-    @SuppressWarnings("deprecation")
-    public static void Show(final Player player, final AdvancedPortalsPlugin plugin) {
-
         int LowX = 0;
         int LowY = 0;
         int LowZ = 0;
@@ -121,31 +42,43 @@ public class Selection {
         int HighY = 0;
         int HighZ = 0;
 
-        if (player.getMetadata("Pos1X").get(0).asInt() > player.getMetadata("Pos2X").get(0).asInt()) {
-            LowX = player.getMetadata("Pos2X").get(0).asInt();
-            HighX = player.getMetadata("Pos1X").get(0).asInt();
+        if (portalName != null) {
+            ConfigAccessor portalConfig = new ConfigAccessor(plugin, "portals.yml");
+
+            LowX = portalConfig.getConfig().getInt(portalName + ".pos2.X");
+            LowY = portalConfig.getConfig().getInt(portalName + ".pos2.Y");
+            LowZ = portalConfig.getConfig().getInt(portalName + ".pos2.Z");
+
+            HighX = portalConfig.getConfig().getInt(portalName + ".pos1.X");
+            HighY = portalConfig.getConfig().getInt(portalName + ".pos1.Y");
+            HighZ = portalConfig.getConfig().getInt(portalName + ".pos1.Z");
         } else {
-            LowX = player.getMetadata("Pos1X").get(0).asInt();
-            HighX = player.getMetadata("Pos2X").get(0).asInt();
-        }
-        if (player.getMetadata("Pos1Y").get(0).asInt() > player.getMetadata("Pos2Y").get(0).asInt()) {
-            LowY = player.getMetadata("Pos2Y").get(0).asInt();
-            HighY = player.getMetadata("Pos1Y").get(0).asInt();
-        } else {
-            LowY = player.getMetadata("Pos1Y").get(0).asInt();
-            HighY = player.getMetadata("Pos2Y").get(0).asInt();
-        }
-        if (player.getMetadata("Pos1Z").get(0).asInt() > player.getMetadata("Pos2Z").get(0).asInt()) {
-            LowZ = player.getMetadata("Pos2Z").get(0).asInt();
-            HighZ = player.getMetadata("Pos1Z").get(0).asInt();
-        } else {
-            LowZ = player.getMetadata("Pos1Z").get(0).asInt();
-            HighZ = player.getMetadata("Pos2Z").get(0).asInt();
+            if (player.getMetadata("Pos1X").get(0).asInt() > player.getMetadata("Pos2X").get(0).asInt()) {
+                LowX = player.getMetadata("Pos2X").get(0).asInt();
+                HighX = player.getMetadata("Pos1X").get(0).asInt();
+            } else {
+                LowX = player.getMetadata("Pos1X").get(0).asInt();
+                HighX = player.getMetadata("Pos2X").get(0).asInt();
+            }
+            if (player.getMetadata("Pos1Y").get(0).asInt() > player.getMetadata("Pos2Y").get(0).asInt()) {
+                LowY = player.getMetadata("Pos2Y").get(0).asInt();
+                HighY = player.getMetadata("Pos1Y").get(0).asInt();
+            } else {
+                LowY = player.getMetadata("Pos1Y").get(0).asInt();
+                HighY = player.getMetadata("Pos2Y").get(0).asInt();
+            }
+            if (player.getMetadata("Pos1Z").get(0).asInt() > player.getMetadata("Pos2Z").get(0).asInt()) {
+                LowZ = player.getMetadata("Pos2Z").get(0).asInt();
+                HighZ = player.getMetadata("Pos1Z").get(0).asInt();
+            } else {
+                LowZ = player.getMetadata("Pos1Z").get(0).asInt();
+                HighZ = player.getMetadata("Pos2Z").get(0).asInt();
+            }
         }
 
         final Location pos1 = new Location(player.getWorld(), LowX, LowY, LowZ);
         final Location pos2 = new Location(player.getWorld(), HighX, HighY, HighZ);
-		
+
 		/*
 		 * There are alot of for loops at the moment, when i find an easier way to do these other that a load of if statements
 		 * then i will change it, but for now its the best way i can think of for doing this. 
