e4f38f50a00ba787d27386faa610538e65daa8ce
Alastair <alastair.hawkes@gmail.com>
2017-07-14 15:03:27 +0100

    fix again

--- src/com/sekwah/advancedportals/api/portaldata/PortalArg.java
+++ src/com/sekwah/advancedportals/api/portaldata/PortalArg.java
@@ -5,7 +5,7 @@ public class PortalArg {
     public final String argName;
     public final String value;
 
-    public PortalArg(Stsring argName, String value/*, int type*/) {
+    public PortalArg(String argName, String value/*, int type*/) {
         this.argName = argName;
         this.value = value;
         // may be used if values need to be 100% not string
