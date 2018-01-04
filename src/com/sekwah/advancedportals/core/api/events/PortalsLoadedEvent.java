d7b5049a44f4a23727099ec14422a49d0a26dc1a
Sekwah <contact@sekwah.com>
2018-01-04 14:51:40 +0000

    Fixed some referenced but still need to finish removing bukkit references

--- src/com/sekwah/advancedportals/core/api/events/PortalsLoadedEvent.java
+++ src/com/sekwah/advancedportals/core/api/events/PortalsLoadedEvent.java
@@ -1,16 +1,16 @@
 package com.sekwah.advancedportals.core.api.events;
 
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
 
 /**
  * Triggered once all the portals are loaded
  *
+ * TODO Need to make a custom event handler to be able to register against
+ *
  * @author sekwah41
  */
-public class PortalsLoadedEvent extends Event {
-    @Override
+public class PortalsLoadedEvent/* extends Event*/ {
+    /*@Override
     public HandlerList getHandlers() {
         return null;
-    }
+    }*/
 }
