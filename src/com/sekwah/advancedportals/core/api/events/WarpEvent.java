c6804bada3892c5d6e594ff5ecf5970a3cddfe8a
Alastair <alastair.hawkes@gmail.com>
2018-01-04 14:51:40 +0000

    Fixed some referenced but still need to finish removing bukkit references

--- src/com/sekwah/advancedportals/core/api/events/WarpEvent.java
+++ src/com/sekwah/advancedportals/core/api/events/WarpEvent.java
@@ -8,10 +8,13 @@ import org.bukkit.event.HandlerList;
 
 /**
  * Trigered whenever a player activates the warp after the tags are handled.
+ *
+ *
+ * TODO Need to make a custom event handler to be able to register against or something similar like a general events data
  */
-public class WarpEvent extends Event implements Cancellable {
+public class WarpEvent /*extends Event implements Cancellable*/ {
 
-    private static HandlerList handlers = new HandlerList();
+    /*private static HandlerList handlers = new HandlerList();
 
     private boolean cancelled = false;
 
@@ -22,20 +25,20 @@ public class WarpEvent extends Event implements Cancellable {
 
     private boolean hasWarped = false;
 
-    /*public WarpEvent(Player player, AdvancedPortal portalData) {
+    *//*public WarpEvent(Player player, AdvancedPortal portalData) {
         this.player = player;
         this.portalData = portalData;
-    }*/
+    }*//*
 
     public static HandlerList getHandlerList() {
         return handlers;
     }
 
-    /**
+    *//**
      * Returns if the event has been cancelled
      *
      * @return cancelled
-     */
+     *//*
     public boolean isCancelled() {
         return cancelled;
     }
@@ -44,24 +47,24 @@ public class WarpEvent extends Event implements Cancellable {
         cancelled = cancel;
     }
 
-    /*public AdvancedPortal getPortalData() {
+    *//*public AdvancedPortal getPortalData() {
         return portalData;
-    }*/
+    }*//*
 
-    /**
+    *//**
      * This will return true if another plugin has warped the player(and set this to true)
      *
      * @return hasWarped
-     */
+     *//*
     public boolean getHasWarped() {
         return hasWarped;
     }
 
-    /**
+    *//**
      * If the
      *
      * @param warped
-     */
+     *//*
     public void setHasWarped(boolean warped) {
         this.hasWarped = warped;
     }
