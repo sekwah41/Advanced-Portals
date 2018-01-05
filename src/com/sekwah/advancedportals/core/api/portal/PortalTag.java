a02b69483083cbff0657b36943c81746bf75d853
Alastair <alastair.hawkes@gmail.com>
2018-01-05 20:47:22 +0000

    removed all bukkit references

--- src/com/sekwah/advancedportals/core/api/portal/PortalTag.java
+++ src/com/sekwah/advancedportals/core/api/portal/PortalTag.java
@@ -4,13 +4,10 @@ public class PortalTag {
 
     public final String NAME;
     public final String VALUE;
-    //public final int type;
 
-    public PortalTag(String argName, String value/*, int type*/) {
+    public PortalTag(String argName, String value) {
         this.NAME = argName;
         this.VALUE = value;
-        // may be used if values need to be 100% not string
-        //this.type = type;
     }
 
 }
