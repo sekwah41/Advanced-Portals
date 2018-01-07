7e7ccc37a76e8ab9d364e95525b43400f0655870
Sekwah <contact@sekwah.com>
2018-01-07 01:19:41 +0000

    Started working on config data class

--- src/com/sekwah/advancedportals/core/AdvancedPortalsCore.java
+++ src/com/sekwah/advancedportals/core/AdvancedPortalsCore.java
@@ -1,5 +1,7 @@
 package com.sekwah.advancedportals.core;
 
+import com.google.gson.Gson;
+import com.sekwah.advancedportals.core.util.Config;
 import com.sekwah.advancedportals.core.util.DataStorage;
 import com.sekwah.advancedportals.core.util.InfoLogger;
 
@@ -9,12 +11,18 @@ public class AdvancedPortalsCore {
     private static CoreConnector coreConnector;
     private static DataStorage dataStorage;
     private static InfoLogger infoLogger;
+    private static Config config;
 
     public AdvancedPortalsCore(CoreConnector coreConnector, DataStorage dataStorage, InfoLogger infoLogger) {
         this.dataStorage = dataStorage;
         this.infoLogger = infoLogger;
         this.instance = this;
         this.coreConnector = coreConnector;
+        this.onEnable();
+    }
+
+    private void onEnable() {
+        config = dataStorage.loadJson(Config.class, "config.json");
     }
 
     private static AdvancedPortalsCore getInstance() {
---
+++ src/com/sekwah/advancedportals/core/util/Config.java
@@ -0,0 +1,31 @@
+package com.sekwah.advancedportals.core.util;
+
+/**
+ * To store the data for config
+ */
+public class Config {
+
+    private boolean useOnlySpecialAxe = false;
+
+    private String axeMaterial = "IRON_AXE";
+
+    private boolean portalProtection = true;
+
+    private int portalProtectionRaduis = 5;
+
+    private String defaultTriggerBlock = "PORTAL";
+
+    private boolean stopWaterFlow = true;
+
+    private String selectionBlock = "STAINED_GLASS";
+
+    private int blockSubID = 14;
+
+    public boolean getUseOnlySpecialAxe() {
+        return useOnlySpecialAxe;
+    }
+
+    public void setUseOnlySpecialAxe(boolean useOnlyServerMadeAxe) {
+        useOnlySpecialAxe = useOnlyServerMadeAxe;
+    }
+}
