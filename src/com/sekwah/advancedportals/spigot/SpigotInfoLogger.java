f9fc7ab47cee9320030e487a02d0bc198bb5ed70
sekwah41 <sekwah41@gmail.com>
2018-01-17 05:41:08 +0000

    Merge branch 'master' into spigot
    # Conflicts:
    #	pom.xml

--- src/com/sekwah/advancedportals/spigot/SpigotInfoLogger.java
+++ src/com/sekwah/advancedportals/spigot/SpigotInfoLogger.java
@@ -1,4 +1,22 @@
 package com.sekwah.advancedportals.spigot;
 
-public class SpigotInfoLogger {
+import com.sekwah.advancedportals.core.util.InfoLogger;
+
+public class SpigotInfoLogger extends InfoLogger {
+
+    private final AdvancedPortalsPlugin plugin;
+
+    public SpigotInfoLogger(AdvancedPortalsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void logWarning(String s) {
+        plugin.getLogger().warning(s);
+    }
+
+    @Override
+    public void log(String s) {
+        plugin.getLogger().info(s);
+    }
 }
