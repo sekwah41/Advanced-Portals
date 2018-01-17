e3265ed98760a0b35f8bc852a52be396d9e0c998
Sekwah <contact@sekwah.com>
2018-01-17 05:45:06 +0000

    Started linking the beginnings of the project

--- src/com/sekwah/advancedportals/spigot/AdvancedPortalsPlugin.java
+++ src/com/sekwah/advancedportals/spigot/AdvancedPortalsPlugin.java
@@ -1,16 +1,19 @@
 package com.sekwah.advancedportals.spigot;
 
 import com.sekwah.advancedportals.core.AdvancedPortalsCore;
+import com.sekwah.advancedportals.core.util.DataStorage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdvancedPortalsPlugin extends JavaPlugin {
 
+    private AdvancedPortalsCore portalsCore;
+
     public void onEnable() {
-        //this.portalsCore = new AdvancedPortalsCore()
+        this.portalsCore = new AdvancedPortalsCore(new DataStorage(this.getDataFolder()), new SpigotInfoLogger(this));
     }
 
     public void onDisable() {
-
+        this.portalsCore.onDisable();
     }
 
 }
