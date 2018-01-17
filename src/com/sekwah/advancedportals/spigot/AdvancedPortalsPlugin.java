5eadca0dc5587b98988d7cebad4af363432c8dc5
sekwah41 <sekwah41@gmail.com>
2018-01-17 05:41:08 +0000

    Merge branch 'master' into spigot

--- src/com/sekwah/advancedportals/spigot/AdvancedPortalsPlugin.java
+++ src/com/sekwah/advancedportals/spigot/AdvancedPortalsPlugin.java
@@ -1,11 +1,12 @@
 package com.sekwah.advancedportals.spigot;
 
+import com.sekwah.advancedportals.core.AdvancedPortalsCore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdvancedPortalsPlugin extends JavaPlugin {
 
     public void onEnable() {
-
+        //this.portalsCore = new AdvancedPortalsCore()
     }
 
     public void onDisable() {
