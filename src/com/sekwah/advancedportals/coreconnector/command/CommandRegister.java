56d46a763f92fd8d8c99bfe3e11db6b2284f18db
sekwah <sekwah41@gmail.com>
2018-01-19 04:14:54 +0000

    Started adding the base of the command registry

---
+++ src/com/sekwah/advancedportals/coreconnector/command/CommandRegister.java
@@ -0,0 +1,19 @@
+package com.sekwah.advancedportals.coreconnector.command;
+
+import com.sekwah.advancedportals.core.commands.CommandTemplate;
+import com.sekwah.advancedportals.core.commands.PortalCommand;
+
+/**
+ * Register the CommandTemplate files to the appropriate system
+ */
+public class CommandRegister {
+
+    /**
+     * Registers the command to the appropriate system
+     * @param commandName
+     * @param commandExecutor
+     */
+    public void registerCommand(String commandName, CommandTemplate commandExecutor) {
+
+    }
+}
