97e329f9d65b9898cf3f4955278a8ea4a42e391e
sekwah <sekwah41@gmail.com>
2018-01-22 21:36:53 +0000

    Starting to add spigot loading

--- src/com/sekwah/advancedportals/spigot/AdvancedPortalsPlugin.java
+++ src/com/sekwah/advancedportals/spigot/AdvancedPortalsPlugin.java
@@ -2,6 +2,7 @@ package com.sekwah.advancedportals.spigot;
 
 import com.sekwah.advancedportals.core.AdvancedPortalsCore;
 import com.sekwah.advancedportals.core.util.DataStorage;
+import com.sekwah.advancedportals.coreconnector.command.CommandRegister;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdvancedPortalsPlugin extends JavaPlugin {
@@ -9,7 +10,8 @@ public class AdvancedPortalsPlugin extends JavaPlugin {
     private AdvancedPortalsCore portalsCore;
 
     public void onEnable() {
-        this.portalsCore = new AdvancedPortalsCore(new DataStorage(this.getDataFolder()), new SpigotInfoLogger(this));
+        this.portalsCore = new AdvancedPortalsCore(new DataStorage(this.getDataFolder()),
+                new SpigotInfoLogger(this), new CommandRegister(this));
     }
 
     public void onDisable() {
