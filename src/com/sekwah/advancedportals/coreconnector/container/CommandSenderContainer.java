06c85f1848f3967df5645f5052b4efbc2ecc3b32
sekwah <sekwah41@gmail.com>
2018-01-22 21:36:53 +0000

    Starting to add spigot loading

--- src/com/sekwah/advancedportals/coreconnector/container/CommandSenderContainer.java
+++ src/com/sekwah/advancedportals/coreconnector/container/CommandSenderContainer.java
@@ -1,8 +1,17 @@
 package com.sekwah.advancedportals.coreconnector.container;
 
 import com.sekwah.advancedportals.core.util.Lang;
+import org.bukkit.command.CommandSender;
 
 public class CommandSenderContainer {
+
+    private final CommandSender sender;
+
+    public CommandSenderContainer(CommandSender commandSender) {
+        this.sender = commandSender;
+    }
+
     public void sendMessage(String message) {
+        sender.sendMessage(message);
     }
 }
