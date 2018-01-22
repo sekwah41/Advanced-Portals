e3fbb80f2ce0ff5f6287e4c99ae831f2aa0579b8
Sekwah <contact@sekwah.com>
2018-01-22 21:36:53 +0000

    Starting to add spigot loading

--- src/com/sekwah/advancedportals/coreconnector/command/CommandRegister.java
+++ src/com/sekwah/advancedportals/coreconnector/command/CommandRegister.java
@@ -1,11 +1,18 @@
 package com.sekwah.advancedportals.coreconnector.command;
 
 import com.sekwah.advancedportals.core.commands.CommandTemplate;
+import com.sekwah.advancedportals.spigot.AdvancedPortalsPlugin;
 
 /**
  * Register the CommandTemplate files to the appropriate system
  */
 public class CommandRegister {
+
+    private final AdvancedPortalsPlugin plugin;
+
+    public CommandRegister(AdvancedPortalsPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     /**
      * Registers the command to the appropriate system
@@ -13,6 +20,6 @@ public class CommandRegister {
      * @param commandExecutor
      */
     public void registerCommand(String commandName, CommandTemplate commandExecutor) {
-
+        this.plugin.getCommand(commandName).setExecutor(new CommandHandler(commandExecutor));
     }
 }
