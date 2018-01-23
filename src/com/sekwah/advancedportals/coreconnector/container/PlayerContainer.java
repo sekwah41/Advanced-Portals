03b534b36c0dc2e8223679c47a7fd5b815378ad6
Sekwah <contact@sekwah.com>
2018-01-23 06:15:14 +0000

    work on the player container

--- src/com/sekwah/advancedportals/coreconnector/container/PlayerContainer.java
+++ src/com/sekwah/advancedportals/coreconnector/container/PlayerContainer.java
@@ -1,18 +1,29 @@
 package com.sekwah.advancedportals.coreconnector.container;
 
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
 /**
  * Just a temporary container for whenever advanced portals needs to get data from a player
  */
 public class PlayerContainer {
 
-    public String getUUID() {
-        return "";
+    private final Player player;
+
+    public PlayerContainer(Player player) {
+        this.player = player;
+    }
+
+    public UUID getUUID() {
+        return player.getUniqueId();
     }
     
     public void sendMessage(String message) {
+        player.sendMessage(message);
     }
 
     public boolean isOp() {
-        return false;
+        return player.isOp();
     }
 }
