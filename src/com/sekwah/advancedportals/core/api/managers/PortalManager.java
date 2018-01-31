64b2d886580113e67688a5fd17e2c489fe2ea9ca
sekwah41 <alastair.hawkes@gmail.com>
2018-01-31 18:06:02 +0000

    Done major work on create command and remove command

--- src/com/sekwah/advancedportals/core/api/managers/PortalManager.java
+++ src/com/sekwah/advancedportals/core/api/managers/PortalManager.java
@@ -3,13 +3,16 @@ package com.sekwah.advancedportals.core.api.managers;
 import com.google.gson.reflect.TypeToken;
 import com.sekwah.advancedportals.core.AdvancedPortalsCore;
 import com.sekwah.advancedportals.core.api.portal.AdvancedPortal;
+import com.sekwah.advancedportals.core.api.portal.PortalTag;
+import com.sekwah.advancedportals.core.api.portal.PortalTagExeption;
+import com.sekwah.advancedportals.core.api.warphandler.TagHandler;
 import com.sekwah.advancedportals.core.data.PlayerLocation;
 import com.sekwah.advancedportals.core.data.PortalLocation;
+import com.sekwah.advancedportals.core.util.Lang;
 import com.sekwah.advancedportals.coreconnector.container.PlayerContainer;
 
 import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * When a player leaves the server any data stored on them is removed to free memory.
@@ -56,17 +59,28 @@ public class PortalManager {
     }
 
 
-
     /**
      * Load the default data into the portals.
      */
     public void loadPortals() {
-        Type type = new TypeToken<Map<String, AdvancedPortal>>(){}.getType();
-        this.portalHashMap = this.portalsCore.getDataStorage().loadJson(type ,"portals.json");
-        if(this.portalHashMap == null) {
+        Type type = new TypeToken<Map<String, AdvancedPortal>>() {
+        }.getType();
+        this.portalHashMap = this.portalsCore.getDataStorage().loadJson(type, "portals.json");
+        if (this.portalHashMap == null) {
             this.portalHashMap = new HashMap<>();
         }
         this.portalsCore.getDataStorage().storeJson(this.portalHashMap, "config.json");
+        this.updatePortalArray();
+    }
+
+    public boolean removePortal(String portalName) {
+        if (this.portalHashMap.containsKey(portalName)) {
+            this.portalHashMap.remove(portalName);
+            this.updatePortalArray();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void playerJoin(PlayerContainer player) {
@@ -79,5 +93,92 @@ public class PortalManager {
 
     public boolean playerMove(PlayerContainer player, PlayerLocation fromLoc, PlayerLocation toLoc) {
         return false;
+    }
+
+    public void createPortal(PlayerContainer player, ArrayList<PortalTag> tags) throws PortalTagExeption {
+        if (this.portalSelectorLeftClick.containsKey(player.getUUID().toString())
+                && this.portalSelectorRightClick.containsKey(player.getUUID().toString())) {
+            this.createPortal(player, this.portalSelectorLeftClick.get(player.getUUID().toString()),
+                    this.portalSelectorRightClick.get(player.getUUID().toString()), tags);
+        } else {
+            throw new PortalTagExeption(Lang.translate("portal.invalidselection"));
+        }
+    }
+
+    /**
+     *
+     * @param loc1
+     * @param loc2
+     * @param tags
+     * @return
+     * @throws PortalTagExeption
+     */
+    public boolean createPortal(PortalLocation loc1, PortalLocation loc2, ArrayList<PortalTag> tags) throws PortalTagExeption {
+        return createPortal(null, loc1, loc2, tags);
+    }
+
+
+    /**
+     * Maybe add detection for overlapping however thats not a major issue for now and portals may overlap inside
+     * solid blocks
+     *
+     * @param player null if no player
+     * @param loc1
+     * @param loc2
+     * @param tags
+     * @return
+     * @throws PortalTagExeption
+     */
+    public boolean createPortal(PlayerContainer player, PortalLocation loc1, PortalLocation loc2, ArrayList<PortalTag> tags) throws PortalTagExeption {
+        int maxX = Math.max(loc1.posX, loc2.posX);
+        int maxY = Math.max(loc1.posY, loc2.posY);
+        int maxZ = Math.max(loc1.posZ, loc2.posZ);
+        int minX = Math.min(loc1.posX, loc2.posX);
+        int minY = Math.min(loc1.posY, loc2.posY);
+        int minZ = Math.min(loc1.posZ, loc2.posZ);
+
+        PortalLocation maxLoc = new PortalLocation(maxX, maxY, maxZ);
+        PortalLocation minLoc = new PortalLocation(minX, minY, minZ);
+
+        AdvancedPortal portal = new AdvancedPortal(maxLoc, minLoc);
+        for(PortalTag portalTag : tags) {
+            portal.setArg(portalTag);
+        }
+        for(PortalTag portalTag : tags) {
+            TagHandler.Creation creation = AdvancedPortalsCore.getTagRegistry().getCreationHandler(portalTag.NAME);
+            creation.portalCreated(portal, player, portalTag.VALUE);
+        }
+        String portalName = portal.getArg("name");
+        if(portalName == null || portalName.equals("")) {
+            throw new PortalTagExeption(Lang.translate("portal.noname"));
+        }
+        else if(this.portalHashMap.containsKey(portalName)) {
+            throw new PortalTagExeption(Lang.translate("portal.takenname"));
+        }
+        portal.removeArg("name");
+        this.portalHashMap.put(portalName, portal);
+        this.updatePortalArray();
+        return true;
+    }
+
+    private void updatePortalArray() {
+        Collection<AdvancedPortal> portalValues = this.portalHashMap.values();
+        this.portals = portalValues.toArray(new AdvancedPortal[0]);
+    }
+
+    public void removePlayerSelection(PlayerContainer player) throws PortalTagExeption {
+
+    }
+
+    public void removePortal(PlayerContainer player, String portalName) throws PortalTagExeption {
+
+    }
+
+    /**
+     * Get an array of portals without a reference to the main array
+     * @return
+     */
+    public Set<Map.Entry<String, AdvancedPortal>> getPortals() {
+        return this.portalHashMap.entrySet();
     }
 }
