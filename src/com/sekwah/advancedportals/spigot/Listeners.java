64548e1a908dc6e00642223abe1c8d583520bf96
Sekwah <contact@sekwah.com>
2018-02-01 21:49:47 +0000

    Updating permissions and such

--- src/com/sekwah/advancedportals/spigot/Listeners.java
+++ src/com/sekwah/advancedportals/spigot/Listeners.java
@@ -1,9 +1,13 @@
 package com.sekwah.advancedportals.spigot;
 
 import com.sekwah.advancedportals.core.CoreListeners;
+import com.sekwah.advancedportals.core.data.PortalLocation;
 import com.sekwah.advancedportals.coreconnector.container.PlayerContainer;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Listeners implements Listener {
@@ -19,6 +23,17 @@ public class Listeners implements Listener {
     @EventHandler
     public void onJoinEvent(PlayerJoinEvent event) {
         coreListeners.playerJoin(new PlayerContainer(event.getPlayer()));
+    }
+
+    @EventHandler
+    public void onItemInteract(PlayerInteractEvent event) {
+        if ((event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_BLOCK) && event.getItem() != null) {
+            Location blockloc = event.getClickedBlock().getLocation();
+            this.coreListeners.playerInteractWithBlock(new PlayerContainer(event.getPlayer()), event.getMaterial().toString(),
+                    event.getItem().getItemMeta().getDisplayName(),
+                    new PortalLocation(blockloc.getBlockX(), blockloc.getBlockY(), blockloc.getBlockZ()),
+                    event.getAction() == Action.LEFT_CLICK_BLOCK);
+        }
     }
 
 }
