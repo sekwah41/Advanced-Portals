ab2410d8892758e7ae810a4d1a76356a008ffb9f
sekwah41 <alastair.hawkes@gmail.com>
2018-02-02 10:38:31 +0000

    Added serializable names

--- src/com/sekwah/advancedportals/core/data/PortalLocation.java
+++ src/com/sekwah/advancedportals/core/data/PortalLocation.java
@@ -1,10 +1,16 @@
 package com.sekwah.advancedportals.core.data;
 
+import com.google.gson.annotations.SerializedName;
+
 public class PortalLocation {
 
+    @SerializedName("x")
     public final int posX;
+    @SerializedName("y")
     public final int posY;
+    @SerializedName("z")
     public final int posZ;
+    @SerializedName("w")
     public final String worldName;
 
     public PortalLocation(String worldName, int posX, int posY, int posZ) {
