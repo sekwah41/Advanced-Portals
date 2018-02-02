568d2517b6f0a2af082b94a0da4cd018dec41959
sekwah41 <alastair.hawkes@gmail.com>
2018-02-02 09:28:20 +0000

    Updating teleport data and other command info

--- src/com/sekwah/advancedportals/coreconnector/container/CommandSenderContainer.java
+++ src/com/sekwah/advancedportals/coreconnector/container/CommandSenderContainer.java
@@ -32,6 +32,6 @@ public class CommandSenderContainer {
     }
 
     public boolean hasPermission(String permission) {
-        return false;
+        return sender.hasPermission(permission);
     }
 }
