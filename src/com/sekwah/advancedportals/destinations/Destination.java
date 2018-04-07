1410ad30289b8738dccf6b34c9a60c3a32fd266f
Sekwah <contact@sekwah.com>
2018-04-07 03:30:55 +0100

    Added support for 1.8 as it was broken, need to fully test with 1.12 and a few other versions before release

--- src/com/sekwah/advancedportals/destinations/Destination.java
+++ src/com/sekwah/advancedportals/destinations/Destination.java
@@ -138,7 +138,7 @@ public class Destination {
                     player.sendMessage(PluginMessages.customPrefixFail + "\u00A7a You have been warped to \u00A7e" + name.replaceAll("_", " ") + "\u00A7a.");
                     player.sendMessage("");
                 } else if (PortalMessagesDisplay == 2) {
-                    plugin.compat.sendActionBarMessage("{\"text\":\"\u00A7aYou have warped to \u00A7e" + name.replaceAll("_", " ") + "\u00A7a.\"}", player);
+                    plugin.compat.sendActionBarMessage("\u00A7aYou have warped to \u00A7e" + name.replaceAll("_", " ") + "\u00A7a.", player);
                     /**plugin.nmsAccess.sendActionBarMessage("[{text:\"You have warped to \",color:green},{text:\"" + config.getConfig().getString(Portal.portals[portalId].portalName + ".destination").replaceAll("_", " ")
                      + "\",color:yellow},{\"text\":\".\",color:green}]", player);*/
                 }
