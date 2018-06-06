8a23ab73187ef323fbdfe9913a014365aae733d9
Sekwah <sekwah41@gmail.com>
2018-06-06 22:08:31 +0100

    Moved the removeplayerselection method

--- src/main/java/com/sekwah/advancedportals/core/api/services/PortalTempDataServices.java
+++ src/main/java/com/sekwah/advancedportals/core/api/services/PortalTempDataServices.java
@@ -1,11 +1,20 @@
 package com.sekwah.advancedportals.core.api.services;
 
-import com.sekwah.advancedportals.core.AdvancedPortalsCore;
 import com.sekwah.advancedportals.core.api.portal.PortalException;
+import com.sekwah.advancedportals.core.entities.PlayerTempData;
 import com.sekwah.advancedportals.core.entities.PortalLocation;
 import com.sekwah.advancedportals.coreconnector.container.PlayerContainer;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public final class PortalTempDataServices {
+
+    /**
+     * Possibly change to the cache map Aztec was talking about
+     */
+    private Map<UUID, PlayerTempData> tempDataMap = new HashMap<>();
     
     public void activateCooldown(PlayerContainer player) {
     }
@@ -14,10 +23,6 @@ public final class PortalTempDataServices {
     }
 
     public void playerSelectorActivate(PlayerContainer player, PortalLocation blockLoc, boolean leftClick) {
-
-    }
-
-    public void removePlayerSelection(PlayerContainer player) throws PortalException {
 
     }
 }
