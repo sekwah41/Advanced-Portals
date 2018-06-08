80dcd4d89756a40a1ce51d3b5b90a8c5441c742b
Sekwah <contact@sekwah.com>
2018-06-08 08:23:08 +0100

    Now gets mc version from spigot

--- src/main/java/com/sekwah/advancedportals/core/data/DataStorage.java
+++ src/main/java/com/sekwah/advancedportals/core/data/DataStorage.java
@@ -138,10 +138,8 @@ public class DataStorage {
         File inFile = new File(dataFolder, location);
         if (inFile.exists() && !inFile.isDirectory()) {
             try {
-                System.out.println("OUTPUT 1");
                 return new FileInputStream(inFile);
             } catch (FileNotFoundException e) {
-                System.out.println("OUTPUT 2");
                 e.printStackTrace();
                 return null;
             }
@@ -150,7 +148,6 @@ public class DataStorage {
                 copyDefaultFile(location, false);
                 return this.getClass().getClassLoader().getResourceAsStream(location);
             } catch (NullPointerException e) {
-                System.out.println("OUTPUT 4");
                 e.printStackTrace();
                 this.portalsCore.getInfoLogger().logWarning("Could not load " + location + ". The file does" +
                         "not exist or there has been an error reading the file.");
