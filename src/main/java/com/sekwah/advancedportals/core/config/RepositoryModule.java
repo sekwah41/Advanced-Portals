a7d49e7eae7e548ef45ccf6a4519aa2fc5a50f18
Sekwah <sekwah41@gmail.com>
2018-07-05 03:18:23 +0100

    Removed unneeded system outs

--- src/main/java/com/sekwah/advancedportals/core/config/RepositoryModule.java
+++ src/main/java/com/sekwah/advancedportals/core/config/RepositoryModule.java
@@ -16,7 +16,6 @@ public class RepositoryModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        System.out.println("Configured");
         bind(PortalRepository.class).to(PortalRepositoryImpl.class).in(Scopes.SINGLETON);
         bind(DestinationRepository.class).to(DestinationRepositoryImpl.class).in(Scopes.SINGLETON);
         bind(PortalTempDataRepository.class).to(PortalTempDataRepositoryImpl.class).in(Scopes.SINGLETON);
@@ -25,7 +24,6 @@ public class RepositoryModule extends AbstractModule {
 
     @Provides
     AdvancedPortalsCore providePortalsCore() {
-        System.out.println("TEST");
         return this.portalsCore;
     }
 }
