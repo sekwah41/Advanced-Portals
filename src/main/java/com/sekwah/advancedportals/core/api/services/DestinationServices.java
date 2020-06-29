851b0688c14ff6011090e6e40e27cf206ca7da5d
Sekwah <sekwah41@gmail.com>
2020-06-29 01:05:14 +0100

    A little cleanup

--- src/main/java/com/sekwah/advancedportals/core/api/services/DestinationServices.java
+++ src/main/java/com/sekwah/advancedportals/core/api/services/DestinationServices.java
@@ -3,12 +3,11 @@ package com.sekwah.advancedportals.core.api.services;
 import com.sekwah.advancedportals.core.api.destination.Destination;
 import com.sekwah.advancedportals.core.data.DataTag;
 import com.sekwah.advancedportals.core.data.PlayerLocation;
-import com.sekwah.advancedportals.repository.DestinationRepositoryOld;
 import com.sekwah.advancedportals.core.connector.container.PlayerContainer;
 
 import java.util.ArrayList; /**
  * https://github.com/sekwah41/Advanced-Portals/blob/24175610892152828e21f4ff824eb1589ccb0338/src/com/sekwah/advancedportals/core/api/managers/DestinationManager.java
- * Based off the old manager with the data storage and handling moved to {@link DestinationRepositoryOld}
+ * Based off the old manager with the data storage and handling moved to
  */
 public final class DestinationServices {
 
