e8cbb403c5237fa4ffb61d5ccc6b50b0f461324e
Sekwah <sekwah41@gmail.com>
2021-05-12 02:52:43 +0100

    ci: Fix issue with wrong dev mode

--- .versionrc.js
+++ .versionrc.js
@@ -1,6 +1,17 @@
 let versionRegex = /(\nversion:\s)([0-9.-]+)/;
 let velocityVersionRegex = /(\sversion\s=\s")([0-9.-]+)("\))/;
 
+
+const ymlUpdater = {
+  updater: {
+    'readVersion': (contents) => {
+      return versionRegex.exec(contents)[2];
+    },
+    'writeVersion': (contents, version) => {
+      return contents.replace(versionRegex, `$1${version}`);
+    }
+  }
+}
 const plugin = {
   filename: 'src/main/resources/plugin.yml',
   updater: {
@@ -11,6 +22,16 @@ const plugin = {
       return contents.replace(versionRegex, `$1${version}`);
     }
   }
+}
+
+const bungee = {
+  filename: 'src/main/resources/bungee.yml',
+  ...ymlUpdater,
+}
+
+const plugin = {
+  filename: 'src/main/resources/plugin.yml',
+  ...ymlUpdater,
 }
 
 const velocity_plugin = {
@@ -25,7 +46,9 @@ const velocity_plugin = {
   }
 }
 
+const files = [plugin, velocity_plugin, bungee];
+
 module.exports = {
-  bumpFiles: [plugin, velocity_plugin],
-  packageFiles: [plugin, velocity_plugin]
+  bumpFiles: files,
+  packageFiles: files
 }
