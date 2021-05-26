9262d50f909d76ab457894a43a0baf46c5c23353
Sekwah <sekwah41@gmail.com>
2021-05-26 02:25:32 +0100

    Started setting up guice

---
+++ core/src/main/java/com/sekwah/advancedportals/core/config/ConfigHandler.java
@@ -0,0 +1,10 @@
+package com.sekwah.advancedportals.core.config;
+
+import com.google.inject.Provider;
+
+public class ConfigHandler implements Provider<Config> {
+    @Override
+    public Config get() {
+        return null;
+    }
+}
