827dd2e30808cbd391e121ea67d666a1f4e79f74
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-11-28 22:47:48 -0500

    feat: Adds ability to configure warp message

--- src/main/java/com/sekwah/advancedportals/bukkit/PluginMessages.java
+++ src/main/java/com/sekwah/advancedportals/bukkit/PluginMessages.java
@@ -1,26 +1,33 @@
 package com.sekwah.advancedportals.bukkit;
 
 import com.sekwah.advancedportals.bukkit.config.ConfigAccessor;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class PluginMessages {
-    private final AdvancedPortalsPlugin plugin;
+    private static String WARP_MESSAGE;
     public boolean useCustomPrefix = false;
     public static String customPrefix = "\u00A7a[\u00A7eAdvancedPortals\u00A7a]";
     public static String customPrefixFail = "\u00A7c[\u00A77AdvancedPortals\u00A7c]";
 
     public PluginMessages (AdvancedPortalsPlugin plugin) {
-        this.plugin = plugin;
-        ConfigAccessor config = new ConfigAccessor(this.plugin, "config.yml");
+        ConfigAccessor config = new ConfigAccessor(plugin, "config.yml");
         this.useCustomPrefix = config.getConfig().getBoolean("UseCustomPrefix");
         if (useCustomPrefix) {
             PluginMessages.customPrefix = config.getConfig().getString("CustomPrefix").replaceAll("&(?=[0-9a-fk-or])", "\u00A7");
             PluginMessages.customPrefixFail = config.getConfig().getString("CustomPrefixFail").replaceAll("&(?=[0-9a-fk-or])", "\u00A7");
         }
+
+        WARP_MESSAGE = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("WarpMessage", "&aYou have warped to &e<warp>&a"));
     }
 
     // This class is so then the common messages in commands or just messages over the commands are the same and can be
     //  easily changed.
+
+    public static String getWarpMessage(String warp) {
+        String cleanedWarp = warp.replace("_", " ");
+        return WARP_MESSAGE.replace("<warp>", cleanedWarp);
+    }
 
     public static void UnknownCommand(CommandSender sender, String command) {
         sender.sendMessage(customPrefixFail + " You need to type something after /" + command + "\n");
