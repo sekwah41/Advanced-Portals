72ab6f94ee4b59799c6be5d025329aba05639a35
Sekwah <contact@sekwah.com>
2021-12-12 14:01:17 +0000

    docs: Update documentation website meta info

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Home`}
+      description="An advanced portals plugin for bukkit">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
