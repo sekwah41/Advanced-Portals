f5765626e24dd2a504fae6c1854e7a1cf4988222
TreemanKing <67459602+TreemanKing@users.noreply.github.com>
2023-09-12 12:58:17 +1000

    fix: add custom messages to plugin
    fix: add custom messages to several options:
    1. Join Cooldown Protection
    2. No Portal Permission
    3. No Portal Cooldown
    
    This is all moved to a new configuration file called messages.yml so that config.yml doesn't become convoluted
    
    Fixes
    #144
    #224

--- src/main/java/com/sekwah/advancedportals/bukkit/PluginMessages.java
+++ src/main/java/com/sekwah/advancedportals/bukkit/PluginMessages.java
@@ -6,12 +6,15 @@ import org.bukkit.command.CommandSender;
 
 public class PluginMessages {
     private static String WARP_MESSAGE;
+    private static String COOLDOWN_PROTECTION_MESSAGE;
+    private static String PORTAL_COOLDOWN_MESSAGE;
+    private static String NO_PERMISSION_PORTAL;
     public boolean useCustomPrefix = false;
     public static String customPrefix = "\u00A7a[\u00A7eAdvancedPortals\u00A7a]";
     public static String customPrefixFail = "\u00A7c[\u00A77AdvancedPortals\u00A7c]";
 
     public PluginMessages (AdvancedPortalsPlugin plugin) {
-        ConfigAccessor config = new ConfigAccessor(plugin, "config.yml");
+        ConfigAccessor config = new ConfigAccessor(plugin, "messages.yml");
         this.useCustomPrefix = config.getConfig().getBoolean("UseCustomPrefix");
         if (useCustomPrefix) {
             PluginMessages.customPrefix = config.getConfig().getString("CustomPrefix").replaceAll("&(?=[0-9a-fk-or])", "\u00A7");
@@ -19,6 +22,9 @@ public class PluginMessages {
         }
 
         WARP_MESSAGE = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("WarpMessage", "&aYou have warped to &e<warp>&a"));
+        COOLDOWN_PROTECTION_MESSAGE = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("CooldownMessage", "&cThere is &e<time>&c join cooldown protection left."));
+        NO_PERMISSION_PORTAL = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("NoPermissionPortal", "&cYou do not have permission to use this portal!"));
+        PORTAL_COOLDOWN_MESSAGE = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("PortalCooldownMessage", "&cPlease wait &e<time> &cuntil attempting to enter this portal again"));
     }
 
     // This class is so then the common messages in commands or just messages over the commands are the same and can be
@@ -27,6 +33,20 @@ public class PluginMessages {
     public static String getWarpMessage(String warp) {
         String cleanedWarp = warp.replace("_", " ");
         return WARP_MESSAGE.replace("<warp>", cleanedWarp);
+    }
+
+    public static String getCooldownProtectionMessage(int time) {
+        String secondsOrSecond = (time == 1) ? " second" : " seconds";
+        return COOLDOWN_PROTECTION_MESSAGE.replace("<time>", time + secondsOrSecond);
+    }
+
+    public static String getPortalCooldownMessage(int time) {
+        String secondsOrSecond = (time == 1) ? " second" : " seconds";
+        return COOLDOWN_PROTECTION_MESSAGE.replace("<time>", time + secondsOrSecond);
+    }
+
+    public static String getNoPermissionPortal() {
+        return NO_PERMISSION_PORTAL;
     }
 
     public static void UnknownCommand(CommandSender sender, String command) {
