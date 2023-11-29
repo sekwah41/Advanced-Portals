19d6d8f0fb120881f0066392408fbafbf490fe83
Sekwah <contact@sekwah.com>
2023-11-29 03:32:11 +0000

    feat: add a warning for no name being included

--- core/src/main/java/com/sekwah/advancedportals/core/services/PortalServices.java
+++ core/src/main/java/com/sekwah/advancedportals/core/services/PortalServices.java
@@ -1,10 +1,13 @@
 package com.sekwah.advancedportals.core.services;
 
 import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
 import com.sekwah.advancedportals.core.connector.containers.PlayerContainer;
 import com.sekwah.advancedportals.core.serializeddata.DataTag;
 import com.sekwah.advancedportals.core.serializeddata.PlayerLocation;
 import com.sekwah.advancedportals.core.portal.AdvancedPortal;
+import com.sekwah.advancedportals.core.util.InfoLogger;
+import com.sekwah.advancedportals.core.util.Lang;
 
 import javax.inject.Singleton;
 import java.util.ArrayList;
@@ -13,6 +16,10 @@ import java.util.Map;
 
 @Singleton
 public class PortalServices {
+
+    @Inject
+    InfoLogger infoLogger;
+
     public void loadPortals() {
 
     }
@@ -34,6 +41,11 @@ public class PortalServices {
     }
 
     public AdvancedPortal createPortal(String name, PlayerContainer player, ArrayList<DataTag> portalTags) {
+        if(name == null){
+            infoLogger.logWarning("Attempted to make a portal with no name");
+            player.sendMessage(Lang.translate("messageprefix.negative") + Lang.translate("command.error.noname"));
+            return null;
+        }
         return null;
     }
 
