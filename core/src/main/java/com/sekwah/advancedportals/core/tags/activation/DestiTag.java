d8f27c27d043c5db945be60e9e36e71a412d99a9
Sekwah <contact@sekwah.com>
2023-12-01 04:11:29 +0000

    feat: adding auto complete for tags

--- core/src/main/java/com/sekwah/advancedportals/core/tags/activation/DestiTag.java
+++ core/src/main/java/com/sekwah/advancedportals/core/tags/activation/DestiTag.java
@@ -2,16 +2,35 @@ package com.sekwah.advancedportals.core.tags.activation;
 
 import com.sekwah.advancedportals.core.connector.containers.PlayerContainer;
 import com.sekwah.advancedportals.core.registry.TagTarget;
+import com.sekwah.advancedportals.core.util.Lang;
 import com.sekwah.advancedportals.core.warphandler.ActivationData;
 import com.sekwah.advancedportals.core.warphandler.Tag;
 
-public class DestiTag implements Tag.Activation {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DestiTag implements Tag.Activation, Tag.AutoComplete {
 
     private final TagType[] tagTypes = new TagType[]{ TagType.PORTAL };
 
     @Override
     public TagType[] getTagTypes() {
         return tagTypes;
+    }
+
+    @Override
+    public String getName() {
+        return "destination";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[]{"desti"};
+    }
+
+    @Override
+    public String description() {
+        return Lang.translate("tag.desti.description");
     }
 
     @Override
@@ -27,5 +46,16 @@ public class DestiTag implements Tag.Activation {
     @Override
     public boolean activated(TagTarget target, PlayerContainer player, ActivationData activeData, String[] argData) {
         return false;
+    }
+
+    @Override
+    public List<String> autoComplete(String argData) {
+        List<String> autoCompletes = new ArrayList<>();
+        // Get all and filter by the argData
+
+        autoCompletes.add("AIR");
+        autoCompletes.add("WATER");
+
+        return autoCompletes;
     }
 }
