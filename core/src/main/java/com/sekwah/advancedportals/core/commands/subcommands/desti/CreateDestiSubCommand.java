c9448f8ec19fb13f8958c8b5319dd4bb509acbbe
Sekwah <contact@sekwah.com>
2023-12-03 05:03:08 +0000

    feat: center titles based on length

--- core/src/main/java/com/sekwah/advancedportals/core/commands/subcommands/desti/CreateDestiSubCommand.java
+++ core/src/main/java/com/sekwah/advancedportals/core/commands/subcommands/desti/CreateDestiSubCommand.java
@@ -1,10 +1,13 @@
 package com.sekwah.advancedportals.core.commands.subcommands.desti;
 
+import com.google.inject.Inject;
 import com.sekwah.advancedportals.core.commands.SubCommand;
 import com.sekwah.advancedportals.core.connector.containers.CommandSenderContainer;
 import com.sekwah.advancedportals.core.connector.containers.PlayerContainer;
+import com.sekwah.advancedportals.core.destination.Destination;
 import com.sekwah.advancedportals.core.serializeddata.DataTag;
 import com.sekwah.advancedportals.core.permissions.PortalPermissions;
+import com.sekwah.advancedportals.core.services.DestinationServices;
 import com.sekwah.advancedportals.core.util.Lang;
 import com.sekwah.advancedportals.core.util.TagReader;
 
@@ -12,6 +15,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CreateDestiSubCommand implements SubCommand {
+
+    @Inject
+    DestinationServices destinationServices;
 
     @Override
     public void onCommand(CommandSenderContainer sender, String[] args) {
@@ -21,25 +27,31 @@ public class CreateDestiSubCommand implements SubCommand {
                 sender.sendMessage(Lang.translate("messageprefix.negative") + Lang.translate("command.createdesti.console"));
                 return;
             }
-            ArrayList<DataTag> destiTags = TagReader.getTagsFromArgs(args);
-            // TODO sort desti service
-            /*Destination desti = AdvancedPortalsCore.getDestinationServices().createDesti(args[1], player, player.getLoc(), destiTags);
-            if(desti != null) {
-                sender.sendMessage(Lang.translateColor("messageprefix.positive") + Lang.translateColor("command.createdesti.complete"));
-                sender.sendMessage(Lang.translateColor("command.create.tags"));
-                ArrayList<DataTag> destiArgs = desti.getArgs();
-                if(destiArgs.size() == 0) {
-                    sender.sendMessage(Lang.translateColor("desti.info.noargs"));
+            ArrayList<DataTag> destinationTags = TagReader.getTagsFromArgs(args);
+
+            Destination destination = destinationServices.createDesti(args[1], player, player.getLoc(), destinationTags);
+            if(destination != null) {
+                sender.sendMessage(Lang.translate("messageprefix.positive") + Lang.translate("command.createdesti.complete"));
+                sender.sendMessage(Lang.translate("command.create.tags"));
+
+                ArrayList<DataTag> destiArgs = destination.getArgs();
+
+                if(destiArgs.isEmpty()) {
+                    sender.sendMessage(Lang.translate("desti.info.noargs"));
                 }
                 else {
                     for (DataTag tag : destiArgs) {
-                        sender.sendMessage("\u00A7a" + tag.NAME + "\u00A77:\u00A7e" + tag.VALUE);
+                        if(tag.VALUES.length == 1) {
+                            sender.sendMessage("\u00A7a" + tag.NAME + "\u00A77:\u00A7e" + tag.VALUES[0]);
+                        } else {
+                            sender.sendMessage("\u00A7a" + tag.NAME + "\u00A77:\u00A7e" + tag.VALUES[0]);
+                        }
                     }
                 }
             }
             else {
-                sender.sendMessage(Lang.translateColor("messageprefix.negative") + Lang.translateColor("command.createdesti.error"));
-            }*/
+                sender.sendMessage(Lang.translate("messageprefix.negative") + Lang.translate("command.createdesti.error"));
+            }
         }
         else {
             sender.sendMessage(Lang.translate("messageprefix.positive") + Lang.translate("command.error.noname"));
--- core/src/main/java/com/sekwah/advancedportals/core/tags/activation/NameTag.java
+++ core/src/main/java/com/sekwah/advancedportals/core/tags/activation/NameTag.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class NameTag implements Tag.Activation, Tag.AutoComplete {
 
-    private final TagType[] tagTypes = new TagType[]{ TagType.PORTAL };
+    private final TagType[] tagTypes = new TagType[]{ TagType.PORTAL, TagType.DESTINATION };
 
     @Override
     public TagType[] getTagTypes() {
