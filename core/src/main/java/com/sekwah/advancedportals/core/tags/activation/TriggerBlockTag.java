917ff8f9fac3839d0408f1f2bb3ab3630d305822
Sekwah <contact@sekwah.com>
2023-12-18 02:13:35 +0000

    fix: improve auto complete for triggerblock

--- core/src/main/java/com/sekwah/advancedportals/core/tags/activation/TriggerBlockTag.java
+++ core/src/main/java/com/sekwah/advancedportals/core/tags/activation/TriggerBlockTag.java
@@ -9,7 +9,9 @@ import com.sekwah.advancedportals.core.util.Lang;
 import com.sekwah.advancedportals.core.warphandler.ActivationData;
 import com.sekwah.advancedportals.core.warphandler.Tag;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class TriggerBlockTag implements Tag.AutoComplete {
@@ -43,8 +45,22 @@ public class TriggerBlockTag implements Tag.AutoComplete {
 
     @Override
     public List<String> autoComplete(String argData) {
+        boolean endsWithComma = argData.endsWith(",");
+        String[] items = argData.split(",");
+        Set<String> existingItems = Arrays.stream(items, 0, endsWithComma ? items.length : items.length - 1)
+                .map(String::trim)
+                .collect(Collectors.toSet());
+
+        String partialInput = endsWithComma ? "" : items[items.length - 1].trim();
+        String baseString = endsWithComma ? argData : argData.substring(0, argData.lastIndexOf(",") + 1);
+
         return serverContainer.getTriggerBlocks().stream()
-                .filter(s -> s.startsWith(argData))
+                // Remove already listed items
+                .filter(s -> !existingItems.contains(s))
+                // Remove items that don't match the currently typed input
+                .filter(s -> s.startsWith(partialInput))
+                // Remap so the auto completes actually show
+                .map(s -> baseString + s)
                 .collect(Collectors.toList());
     }
 }
