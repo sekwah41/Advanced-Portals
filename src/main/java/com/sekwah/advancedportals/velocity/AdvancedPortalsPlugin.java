f4887e3f0e4020e57bcc8814aa4dce7dbcb752f6
Sekwah <contact@sekwah.com>
2024-07-17 03:19:44 +0000

    release version: 0.10.0 [skip ci]

--- src/main/java/com/sekwah/advancedportals/velocity/AdvancedPortalsPlugin.java
+++ src/main/java/com/sekwah/advancedportals/velocity/AdvancedPortalsPlugin.java
@@ -23,7 +23,7 @@ import java.util.concurrent.TimeUnit;
  */
 @Plugin(authors = {"sekwah41"} ,id = "advancedportals", name = "Advanced Portals",
         url = "https://www.spigotmc.org/resources/advanced-portals.14356/",
-        version = "0.9.3")
+        version = "0.10.0")
 public class AdvancedPortalsPlugin {
 
     public HashMap<String, String[]> PlayerDestiMap = new HashMap<>();
