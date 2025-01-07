ef6f97615f7c177564ac10c4989e386dabd60f80
TreemanKing <67459602+TreemanKing@users.noreply.github.com>
2025-01-08 08:44:13 +1100

    fix: trigger block validation (#510)

--- core/src/main/java/com/sekwah/advancedportals/core/tags/TriggerBlockTag.java
+++ core/src/main/java/com/sekwah/advancedportals/core/tags/TriggerBlockTag.java
@@ -1,13 +1,15 @@
 package com.sekwah.advancedportals.core.tags;
 
 import com.google.inject.Inject;
+import com.sekwah.advancedportals.core.connector.containers.PlayerContainer;
 import com.sekwah.advancedportals.core.connector.containers.ServerContainer;
+import com.sekwah.advancedportals.core.registry.TagTarget;
 import com.sekwah.advancedportals.core.util.Lang;
 import com.sekwah.advancedportals.core.warphandler.Tag;
 import java.util.List;
-import javax.annotation.Nullable;
 
-public class TriggerBlockTag implements Tag.AutoComplete, Tag.Split {
+public class TriggerBlockTag implements Tag.AutoComplete, Tag.Split, Tag.Creation {
+
     @Inject
     private ServerContainer serverContainer;
 
@@ -38,14 +40,38 @@ public class TriggerBlockTag implements Tag.AutoComplete, Tag.Split {
     @Override
     public List<String> autoComplete(String argData) {
         var triggerBlocks = serverContainer.getCommonTriggerBlocks()
-                                .stream()
-                                .filter(block -> block.contains(argData))
-                                .toList();
+                .stream()
+                .filter(block -> block.contains(argData))
+                .toList();
 
         if (triggerBlocks.isEmpty()) {
             return serverContainer.getAllTriggerBlocks();
         }
 
         return triggerBlocks;
+    }
+
+    @Override
+    public boolean created(TagTarget target, PlayerContainer player, String[] argData) {
+        for (int i = 0; i < argData.length; i++) {
+            String material = serverContainer.matchMaterialName(argData[i]);
+            if (material == null || !isValidMaterial(material)) {
+                player.sendMessage(Lang.getNegativePrefix() + Lang.translate("tag.triggerblock.error.invalidmaterial"));
+                return false;
+            }
+            argData[i] = material;
+        }
+        return true;
+    }
+
+    @Override
+    public void destroyed(TagTarget target, PlayerContainer player, String[] argData) {
+
+    }
+
+    private boolean isValidMaterial(String material) {
+        return serverContainer.getAllTriggerBlocks()
+                .stream()
+                .anyMatch(validMaterial -> validMaterial.equalsIgnoreCase(material));
     }
 }
