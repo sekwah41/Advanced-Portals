a68e77c3cff6ae26db251c3b89ab973a1f4177c0
Sekwah <contact@sekwah.com>
2025-05-09 18:15:56 +0100

    wip: entity update

--- neoforge-1-21/src/neoforge/main/java/com/sekwah/advancedportals/forge/Events.java
+++ neoforge-1-21/src/neoforge/main/java/com/sekwah/advancedportals/forge/Events.java
@@ -1,7 +1,16 @@
 package com.sekwah.advancedportals.forge;
 
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingFallEvent;
+
 /**
  * Events to wrap and pass to AP
  */
+@EventBusSubscriber(modid = AdvancedPortalsMod.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class Events {
+    @SubscribeEvent
+    public static void onEntityUpdate(LivingFallEvent event) {
+        // Do chakra updates and other stuff here unless handled in capabilities
+    }
 }
